8a20510fc39e8382cc8953294506ee63a360313c
Rocky777 <mahenderkumr@gmail.com>
2013-07-22 19:21:26 +0530

    Sample JSF Spring integrated Application

---
+++ JsfWithRichFacesAndSpringInteration/Readme.txt
@@ -0,0 +1,1 @@
+This is a sample jsf application with integrated spring and Richfaces configured
---
+++ JsfWithRichFacesAndSpringInteration/src/main/java/com/rocky/jsfspring/RichBeanInterface.java
@@ -0,0 +1,7 @@
+package com.rocky.jsfspring;
+
+public interface RichBeanInterface {
+
+
+	public String hello();
+}
